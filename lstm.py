009d956a284c0b91a427eb87b23f786ae32605db
cvaldez98 <carlos_nhi@outlook.com>
2019-05-22 19:17:28 +0200

    added plotting in lstm.py to showcase the different pitches for each genre

--- lstm.py
+++ lstm.py
@@ -11,6 +11,8 @@ from keras.layers import LSTM
 from keras.layers import Activation
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
+from matplotlib import pyplot as plt
+from matplotlib.font_manager import FontProperties
 
 def train_network():
     """ Train a Neural Network to generate music """
@@ -28,28 +30,61 @@ def train_network():
 def get_notes():
     """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     notes = []
-
-    for file in glob.glob("rock_music/*.mid"):
+    pitch_occurrences = dict()
+    for file in glob.glob("minecraft_5_songs/*.mid"):
         midi = converter.parse(file)
-
+        
         print("Parsing %s" % file)
 
         notes_to_parse = None
 
         try: # file has instrument parts
             s2 = instrument.partitionByInstrument(midi)
+            print("s2: ", s2)
+            print(len(s2.parts[0]))
             notes_to_parse = s2.parts[0].recurse()
         except: # file has notes in a flat structure
             notes_to_parse = midi.flat.notes
 
         for element in notes_to_parse:
             if isinstance(element, note.Note):
+                # print("element pitch: ", str(element.pitch))
+                if(str(element.pitch) not in pitch_occurrences):
+                    pitch_occurrences[str(element.pitch)] = 1
+                else:
+                    pitch_occurrences[str(element.pitch) ] += 1
                 notes.append(str(element.pitch))
             elif isinstance(element, chord.Chord):
+                s = '.'.join(str(n) for n in element.normalOrder)
+                # print("chord: ", s)
                 notes.append('.'.join(str(n) for n in element.normalOrder))
+            else: 
+                try: 
+                    print("element neither chord or note: ", element)
+                except:
+                    print("yike")
+                    pass
+        
+
+    
 
     with open('data/notes', 'wb') as filepath:
         pickle.dump(notes, filepath)
+    c = 0
+    for key in pitch_occurrences:
+        # print("label: ", key)
+        # print("plotting at index c: ", c)
+        # print("num occurrences: ", pitch_occurrences[key])
+        if(pitch_occurrences[key] > 20):
+            plt.bar(c, int(pitch_occurrences[key]) , width = 0.9, label = str(key), color = numpy.random.rand(3,))
+        # plt.subplots_adjust(bottom= 0.2, top = 0.98)
+        
+        c += 1
+    fontP = FontProperties()
+    fontP.set_size('small')
+    # plt.legend([plot1], "title", prop=fontP)
+    plt.legend(prop=fontP)
+    plt.show()
 
     return notes
 
